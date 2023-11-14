65c68681b574ab9d9eefb7b66c3b547322df9a61
luksim01 <145286746+luksim01@users.noreply.github.com>
2023-11-14 15:21:26 +0000

    cars using forces

--- Assets/Scripts/PlayerControllerP1.cs
+++ Assets/Scripts/PlayerControllerP1.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float speed = 20.0f;
+    [SerializeField] float horsePower = 30000.0f;
     [SerializeField] float turnSpeed = 45.0f;
     [SerializeField] float horizontalInput;
     [SerializeField] float forwardInput;
 
+    private Rigidbody player1Rb;
+    [SerializeField] GameObject centerOfMass;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player1Rb = GetComponent<Rigidbody>();
+        player1Rb.centerOfMass = centerOfMass.transform.localPosition;
     }
 
     // Update is called once per frame
@@ -22,7 +26,8 @@ public class PlayerController : MonoBehaviour
         forwardInput = Input.GetAxis("VerticalP1");
 
         // Moves the vehicle forward based on vertical input
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        player1Rb.AddRelativeForce(Vector3.forward * horsePower * forwardInput);
         // Rotates the vehicle based on horizontal input
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
--- Assets/Scripts/PlayerControllerP2.cs
+++ Assets/Scripts/PlayerControllerP2.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class PlayerControllerP2 : MonoBehaviour
 {
-    [SerializeField] float speed = 20.0f;
+    [SerializeField] float horsePower = 30000.0f;
     [SerializeField] float turnSpeed = 45.0f;
     [SerializeField] float horizontalInput;
     [SerializeField] float forwardInput;
 
+    private Rigidbody player2Rb;
+    [SerializeField] GameObject centerOfMass;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player2Rb = GetComponent<Rigidbody>();
+        player2Rb.centerOfMass = centerOfMass.transform.localPosition;
     }
 
     // Update is called once per frame
@@ -22,7 +26,8 @@ public class PlayerControllerP2 : MonoBehaviour
         forwardInput = Input.GetAxis("VerticalP2");
 
         // Moves the vehicle forward based on vertical input
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        player2Rb.AddRelativeForce(Vector3.forward * horsePower * forwardInput);
         // Rotates the vehicle based on horizontal input
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
